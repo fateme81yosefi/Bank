efca2eb89be8665205c8c18dbdbff7e2d5a007ff
fateme yosefi <ftm.ysfi8888@gmail.com>
2021-06-26 23:37:12 -0700

    add methods

---
+++ src/Core/Bank.java
@@ -0,0 +1,11 @@
+package Core;
+
+import java.util.ArrayList;
+
+public class Bank {
+    ArrayList<User> users = new ArrayList<User>();
+
+    public int reduce(double mojodi,double mablagh){}
+    public int increase(double mojodi,double mablagh){}
+
+}

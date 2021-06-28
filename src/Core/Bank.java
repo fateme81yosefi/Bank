a86e97f3088a59669ec2aa1b4cecbfe38c2c1237
fateme yosefi <ftm.ysfi8888@gmail.com>
2021-06-28 00:09:58 -0700

    debug methods

--- src/Core/Bank.java
+++ src/Core/Bank.java
@@ -34,28 +34,23 @@ public class Bank {
             }
         }return -1;
     }
-    public void reduce(double mojodi, double mablagh) {
-        mojodi -= mablagh;
-    }
-
-    public void increase(double mojodi, double mablagh) {
-        mojodi += mablagh;
-    }
 
     public void addUser(String name, long codemelli, String password, int phoneNum, String email) {
         User user = new User(name, codemelli, password, phoneNum, email);
         users.add(user);
     }
 
-    public void addAcc(Account account) {
+    public void addAcc(Account account,User loginUser) {
         loginUser.accounts.add(account);
     }
-    public void mashahede(long AccNum){
+
+
+    public void mashahede(long AccNum,User loginUser){
         for (int i = 0; i < loginUser.accounts.size(); i++) {
             if (loginUser.accounts.get(i).accNumber==AccNum) {
                 logAcc1=loginUser.accounts.get(i);
                 ///////PRINT INFO /////////
-    }
+            }
         }
     }
     public void manageAcc(){
@@ -63,11 +58,11 @@ public class Bank {
         }
 
 
-    public void setAlias(String alias, long accNum) {
+    public void setAlias(String alias, long accNum,Account logAcc) {
         logAcc.alias=alias;
     }
 
-    public int enteghalVajh(double mablagh,long accNum) {
+    public int enteghalVajh(double mablagh,long accNum,Account logAcc,User loginUser) {
         if (mablagh>logAcc.mojodi)return 0;
         else{
             logAcc.mojodi-=mablagh;
@@ -78,13 +73,13 @@ public class Bank {
                 Tarakonesh tarakonesh=new Tarakonesh(Tarakonesh.TarakoneshType.BARDASHT,date);
                  logAcc.tarakoneshes.add(tarakonesh);
                 Tarakonesh tarakonesh1=new Tarakonesh(Tarakonesh.TarakoneshType.VARIZ,date);
-                logAcc1.tarakoneshes.add(tarakonesh1);
+                loginUser.accounts.get(x).tarakoneshes.add(tarakonesh1);
             return 1;
             }else return -1;
         }
     }
 
-    public int pardakhtGhoboz(long shenaseGhabz,long ShenasePardakht,double mablagh) {
+    public int pardakhtGhoboz(long shenaseGhabz,long ShenasePardakht,double mablagh,Account logAcc) {
         if (mablagh>logAcc.mojodi)return 0;
         else{
             logAcc.mojodi-=mablagh;
@@ -94,17 +89,18 @@ public class Bank {
             return 1;
     }
     }
+/////////////////////////IN METHOD CHECK BESHE FALSE/////////////////////////////////
 
-    public void darkhastVam(double mablagh,int month) {
+    public void darkhastVam(double mablagh,int month,Account logAcc) {
              logAcc.mojodi+=mablagh;
         double mablaghBardashtiMahane=mablagh/month;
            Date dateVamGerefte=new Date();
                 Tarakonesh tarakonesh2=new Tarakonesh(Tarakonesh.TarakoneshType.VARIZ_VAM,dateVamGerefte);
-                logAcc1.tarakoneshes.add(tarakonesh2);
+                logAcc.tarakoneshes.add(tarakonesh2);
 
 
     }
-    public void checkVamTime(Date date, double mablaghBardashtiMahane){///////////INO CHECK KON BAD/////////
+    public void checkVamTime(Date date, double mablaghBardashtiMahane,Account logAcc){
         Date dateGozashteShode=new Date();
         long zamanGozashte=dateGozashteShode.getTime();
         long zamanVam=date.getTime();
@@ -115,11 +111,21 @@ public class Bank {
 
         }
     }
-
-    public void closeAcc(long accNum,String pass) {
+///////////////////////////////////////////////////////////////////////////////////////
+    public void closeAcc(long accNum,long accNumMaghsad,String pass,User loginUser) {
         for (int i = 0; i < loginUser.accounts.size(); i++) {
             if (loginUser.accounts.get(i).accNumber==accNum && loginUser.accounts.get(i).passwordAcc==pass) {
-              loginUser.accounts.remove(i);
+                if (loginUser.accounts.get(i).mojodi==0)
+                loginUser.accounts.remove(i);
+              else {
+                    int e=getIndexAcc1(accNumMaghsad);
+                    loginUser.accounts.get(i).mojodi+=loginUser.accounts.get(i).mojodi;
+                    Date date=new Date();
+                    Tarakonesh tarakonesh=new Tarakonesh(Tarakonesh.TarakoneshType.VARIZ,date);
+                    loginUser.accounts.get(i).tarakoneshes.add(tarakonesh);
+                    loginUser.accounts.get(i).mojodi=0;
+                    loginUser.accounts.remove(i);
+                }
             }
         } }
 

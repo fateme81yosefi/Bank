74a6f9a02831880e0643d273b1bd27c735756e78
fateme yosefi <ftm.ysfi8888@gmail.com>
2021-06-29 05:43:17 -0700

    compelete methods :)

--- src/Core/Bank.java
+++ src/Core/Bank.java
@@ -15,10 +15,10 @@ public class Bank {
         return -1;
     }
 
-    public static int getIndexAcc(String codemelli,String accNum, String pass) {
+    public static int getIndexAcc(String codemelli,String accNum) {
         int g=getIndexUser(codemelli);
         for (int i = 0; i < users.get(g).accounts.size(); i++) {
-            if (users.get(g).accounts.get(i).accNumber==Long.parseLong(accNum) && users.get(g).accounts.get(i).passwordAcc.equals(pass)) {
+            if (users.get(g).accounts.get(i).accNumber==Long.parseLong(accNum)) {
                 return i;
             }
         }return -1;
@@ -36,11 +36,18 @@ public class Bank {
     }
 
 
-    public void mashahede(long AccNum,User loginUser){
-        for (int i = 0; i < loginUser.accounts.size(); i++) {
-            if (loginUser.accounts.get(i).accNumber==AccNum) {
-                logAcc1=loginUser.accounts.get(i);
-                ///////PRINT INFO /////////
+    public void mashahede(String AccNum,String codemelli){
+        int idex=getIndexUser(codemelli);
+        for (int i = 0; i < users.get(idex).accounts.size(); i++) {
+            if (users.get(idex).accounts.get(i).accNumber==Long.parseLong(AccNum)) {
+                System.out.println(users.get(idex).accounts.get(i).accNumber+" , "+users.get(idex).accounts.get(i).mojodi+" , "+
+                                   " , "+users.get(idex).accounts.get(i).accType);
+                for (int v=0;v<users.get(idex).accounts.size();v++){
+                    for (int a=0;a<users.get(idex).accounts.get(v).tarakoneshes.size();a++){
+                        System.out.println("tarakonesh "+a+" account "+v+" :"+users.get(idex).accounts.get(v).tarakoneshes.get(a));
+                    }
+                }
+
             }
         }
     }
@@ -49,83 +56,97 @@ public class Bank {
         }
 
 
-    public void setAlias(String alias, long accNum,Account logAcc) {
-        logAcc.alias=alias;
-    }
-
-    public int enteghalVajh(double mablagh,long accNum,Account logAcc,User loginUser) {
-        if (mablagh>logAcc.mojodi)return 0;
-        else{
-            logAcc.mojodi-=mablagh;
-            int x=getIndexAcc1(accNum);
-            if (x!=-1){
-                (loginUser.accounts.get(x).mojodi)+=mablagh;
-                Date date=new Date();
-                Tarakonesh tarakonesh=new Tarakonesh(Tarakonesh.TarakoneshType.BARDASHT,date);
-                 logAcc.tarakoneshes.add(tarakonesh);
-                Tarakonesh tarakonesh1=new Tarakonesh(Tarakonesh.TarakoneshType.VARIZ,date);
-                loginUser.accounts.get(x).tarakoneshes.add(tarakonesh1);
-            return 1;
-            }else return -1;
+    public void setAlias(String alias, String accNum,String codemelli) {
+        int idex=getIndexUser(codemelli);
+        for (int i = 0; i < users.get(idex).accounts.size(); i++) {
+            if (users.get(idex).accounts.get(i).accNumber==Long.parseLong(accNum)) {
+                users.get(idex).accounts.get(i).alias=alias;
+            }
         }
     }
 
-    public int pardakhtGhoboz(long shenaseGhabz,long ShenasePardakht,double mablagh,Account logAcc) {
-        if (mablagh>logAcc.mojodi)return 0;
+    public int enteghalVajh(String mablagh,String accNumMaghsad,String accNumMabda , String codemelliMada, String codemelliMaghsad) {
+        int idex=getIndexUser(codemelliMada);
+        int idex1=getIndexUser(codemelliMaghsad);
+        int mabda=getIndexAcc(codemelliMada,accNumMabda);
+        int maghsad=getIndexAcc(codemelliMaghsad,accNumMaghsad);
+        if (idex==-1||idex1==-1||mabda==-1||maghsad==-1){
+            System.out.println("شماره حساب یا کد ملی ها درست نیست !");
+            return 0;
+        }
+        if (users.get(idex).accounts.get(mabda).mojodi<Integer.parseInt(mablagh)){
+            System.out.println("موجودی کافی در حساب مبدا برای انتقال وجه وجود ندارد!");
+            return -1;
+        }
+        users.get(idex).accounts.get(mabda).mojodi-=Integer.parseInt(mablagh);
+        users.get(idex1).accounts.get(maghsad).mojodi+=Integer.parseInt(mablagh);
+        Date date=new Date();
+        Tarakonesh tarakoneshMaghsad=new Tarakonesh(Tarakonesh.TarakoneshType.VARIZ,date);
+        Tarakonesh tarakoneshMabda=new Tarakonesh(Tarakonesh.TarakoneshType.BARDASHT,date);
+        users.get(idex1).accounts.get(maghsad).tarakoneshes.add(tarakoneshMaghsad);
+        users.get(idex).accounts.get(mabda).tarakoneshes.add(tarakoneshMabda);
+            return 1;
+            }
+
+
+    public int pardakhtGhoboz(String shenaseGhabz,String ShenasePardakht,String mablagh,String codemelli,String accNum) {
+        int idex=getIndexUser(codemelli);
+        int mabda=getIndexAcc(codemelli,accNum);
+
+        if (Integer.parseInt(mablagh)>users.get(idex).accounts.get(mabda).mojodi) {
+            System.out.println("موجودی کافی نیست !");
+            return 0;
+        }
         else{
-            logAcc.mojodi-=mablagh;
+            users.get(idex).accounts.get(mabda).mojodi-=Integer.parseInt(mablagh);
             Date date=new Date();
             Tarakonesh tarakonesh=new Tarakonesh(Tarakonesh.TarakoneshType.BARDASHT,date);
-            logAcc.tarakoneshes.add(tarakonesh);
+            users.get(idex).accounts.get(mabda).tarakoneshes.add(tarakonesh);
             return 1;
     }
     }
 /////////////////////////IN METHOD CHECK BESHE FALSE/////////////////////////////////
 
-    public void darkhastVam(double mablagh,int month,Account logAcc) {
-             logAcc.mojodi+=mablagh;
-        double mablaghBardashtiMahane=mablagh/month;
+    public void darkhastVam(String mablagh,String month,String codemelli,String accNum) {
+        int idex=getIndexUser(codemelli);
+        int mabda=getIndexAcc(codemelli,accNum);
+        users.get(idex).accounts.get(mabda).mojodi+=Integer.parseInt(mablagh);
+        double mablaghBardashtiMahane=Integer.parseInt(mablagh)/Integer.parseInt(month);
            Date dateVamGerefte=new Date();
                 Tarakonesh tarakonesh2=new Tarakonesh(Tarakonesh.TarakoneshType.VARIZ_VAM,dateVamGerefte);
-                logAcc.tarakoneshes.add(tarakonesh2);
-
+                users.get(idex).accounts.get(mabda).tarakoneshes.add(tarakonesh2);
 
     }
-    public void checkVamTime(Date date, double mablaghBardashtiMahane,Account logAcc){
+    /////////////////////////////////////IN BUGE DARE/////////////////////////////////////////////////////
+    public void checkVamTime(Date date, double mablaghBardashtiMahane,String codemelli,String accNum){
+        int idex=getIndexUser(codemelli);
+        int mabda=getIndexAcc(codemelli,accNum);
         Date dateGozashteShode=new Date();
         long zamanGozashte=dateGozashteShode.getTime();
         long zamanVam=date.getTime();
         if (((zamanGozashte-zamanVam)*1000)%(3600*24*30)==0){
-            logAcc.mojodi-=mablaghBardashtiMahane;
+
+            users.get(idex).accounts.get(mabda).mojodi-=mablaghBardashtiMahane;
+
             Tarakonesh tarakonesh=new Tarakonesh(Tarakonesh.TarakoneshType.BARDASHT_MAHANE_VAM,dateGozashteShode);
-            logAcc.tarakoneshes.add(tarakonesh);
+            users.get(idex).accounts.get(mabda).tarakoneshes.add(tarakonesh);
 
         }
     }
-///////////////////////////////////////////////////////////////////////////////////////
-    public void closeAcc(long accNum,long accNumMaghsad,String pass,User loginUser) {
-        for (int i = 0; i < loginUser.accounts.size(); i++) {
-            if (loginUser.accounts.get(i).accNumber==accNum && loginUser.accounts.get(i).passwordAcc==pass) {
-                if (loginUser.accounts.get(i).mojodi==0)
-                loginUser.accounts.remove(i);
-              else {
-                    int e=getIndexAcc1(accNumMaghsad);
-                    loginUser.accounts.get(i).mojodi+=loginUser.accounts.get(i).mojodi;
-                    Date date=new Date();
-                    Tarakonesh tarakonesh=new Tarakonesh(Tarakonesh.TarakoneshType.VARIZ,date);
-                    loginUser.accounts.get(i).tarakoneshes.add(tarakonesh);
-                    loginUser.accounts.get(i).mojodi=0;
-                    loginUser.accounts.remove(i);
-                }
-            }
-        } }
 
-    public void setLogOutUser(){
-        this.loginUser=null;
-    }
-    public void setLogOutAcc(){
-        this.logAcc=null;
-    }
+
+
+    public void closeAcc(String accNum,String accNumMaghsad,String pass,String codemelli,String codemelliMaghsad) {
+        int idex=getIndexUser(codemelli);
+        int idex1=getIndexUser(codemelliMaghsad);
+        int mabda=getIndexAcc(codemelli,accNum);
+        int maghsad=getIndexAcc(codemelliMaghsad,accNumMaghsad);
+        if (users.get(idex).accounts.get(mabda).mojodi!=0){
+            String s=String.valueOf(users.get(idex).accounts.get(mabda).mojodi);
+            enteghalVajh(s,accNumMaghsad,accNum,codemelli,codemelliMaghsad);
+        }else users.get(idex).accounts.remove(mabda);
+        }
+
 
 ///////////////////////////////////////ADMIN METHODS/////////////////////////////////////////
 
@@ -147,34 +168,34 @@ public class Bank {
     public void editUserMojodi(String codemelli,String accNum,String newMojodi){
         long newMojo=Long.parseLong(newMojodi);
         int idex=getIndexUser(codemelli);
-        int indexAcc1=getIndexAcc1(Long.parseLong(accNum));
+        int indexAcc1=getIndexAcc(codemelli,accNum);
         Date date=new Date();
         Tarakonesh tarakonesh=new Tarakonesh(Tarakonesh.TarakoneshType.EDIT_MOJODI_BY_ADMIN,date);
         users.get(idex).accounts.get(indexAcc1).mojodi=newMojo;
         users.get(idex).accounts.get(indexAcc1).tarakoneshes.add(tarakonesh);
     }
 
-    public void enteghalVajhByAdmin(String codemelli,String accNumMabda,String accNumMaghsad,String mablagh){
-        int idex=getIndexUser(codemelli);
-        int mabda=getIndexAcc1(Long.parseLong(accNumMabda));
-        int maghsad=getIndexAcc1(Long.parseLong(accNumMaghsad));
+    public void enteghalVajhByAdmin(String codemelliMada,String codemelliMaghsad,String accNumMabda,String accNumMaghsad,String mablagh){
+        int idex=getIndexUser(codemelliMada);
+        int idex1=getIndexUser(codemelliMaghsad);
+        int mabda=getIndexAcc(codemelliMada,accNumMabda);
+        int maghsad=getIndexAcc(codemelliMaghsad,accNumMaghsad);
         users.get(idex).accounts.get(mabda).mojodi-=Integer.parseInt(mablagh);
-        users.get(idex).accounts.get(maghsad).mojodi+=Integer.parseInt(mablagh);
+        users.get(idex1).accounts.get(maghsad).mojodi+=Integer.parseInt(mablagh);
         Date date=new Date();
         Tarakonesh tarakoneshMaghsad=new Tarakonesh(Tarakonesh.TarakoneshType.VARIZ,date);
         Tarakonesh tarakoneshMabda=new Tarakonesh(Tarakonesh.TarakoneshType.BARDASHT,date);
-        users.get(idex).accounts.get(maghsad).tarakoneshes.add(tarakoneshMaghsad);
+        users.get(idex1).accounts.get(maghsad).tarakoneshes.add(tarakoneshMaghsad);
         users.get(idex).accounts.get(mabda).tarakoneshes.add(tarakoneshMabda);
     }
     public void clossAccByAdmin(String codemelli,String accNum){
         int idex=getIndexUser(codemelli);
-        int indexAcc=getIndexAcc1(Long.parseLong(accNum));
+        int indexAcc=getIndexAcc(codemelli,accNum);
         users.get(idex).accounts.remove(indexAcc);
     }
 
     public void creatAccByAdmin(String codemelli, String accNum, String pass, Account.AccType Type){
         int idex=getIndexUser(codemelli);
-        int indexAcc=getIndexAcc1(Long.parseLong(accNum));
         Account account=new Account(pass,Type);
         users.get(idex).accounts.add(account);
     }
